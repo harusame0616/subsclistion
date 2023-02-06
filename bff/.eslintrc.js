861f6340a40da10dc57b8b20edf6a1c026841e0b
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-02-07 08:10:01 +0900

    airbnb 導入

--- bff/.eslintrc.js
+++ bff/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'airbnb-base',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -21,5 +22,18 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          'test.{ts,tsx}',
+          'test-*.{ts,tsx}',
+          '**/*{.,_}{test,spec}.{ts,tsx}',
+          '**/jest.config.ts',
+          '**/jest.setup.ts',
+        ],
+        optionalDependencies: false,
+      },
+    ],
   },
 };
