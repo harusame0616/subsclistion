d0f3a391727082c660c848552722a97eb12b5516
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-02-12 17:32:05 +0900

    API へのプロキシ設定追加

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
+  vite: {
+    server: {
+      proxy: {
+        '/graphql': {
+          target: 'http://localhost:3001/graphql',
+        },
+      },
+    },
+  },
 });
