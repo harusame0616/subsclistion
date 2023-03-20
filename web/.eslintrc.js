b78ebb652af5d2dfe6265416bb3dc05524d35a93
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-03-21 05:54:16 +0900

    storybook 初期化

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,26 +1,25 @@
 module.exports = {
   root: true,
-
   extends: [
     'airbnb-base',
     'airbnb-typescript/base',
     '@nuxt/eslint-config',
     'prettier',
+    'plugin:storybook/recommended',
   ],
-
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: 'tsconfig.json',
   },
-
   ignorePatterns: ['.eslintrc.js'],
-
-  rules: { 'import/prefer-default-export': 'off' },
-
+  rules: {
+    'import/prefer-default-export': 'off',
+  },
   settings: {
     'import/resolver': {
       typescript: {
-        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        alwaysTryTypes: true,
+        // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
         project: 'tsconfig.json',
       },
     },
