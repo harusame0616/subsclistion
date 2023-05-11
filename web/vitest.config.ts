3080bc70dfbe92586c4789a95a4699f33bcbb9e9
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-05-11 21:34:15 +0900

    msw 設定

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -1,9 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineVitestConfig } from 'nuxt-vitest/config';
-import AutoImport from 'unplugin-auto-import/vite';
 
 export default defineVitestConfig({
-  plugins: [AutoImport({ imports: [{ '@testing-library/vue': ['render'] }] })],
   test: {
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
